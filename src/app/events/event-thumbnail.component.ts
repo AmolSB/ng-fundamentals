d0dc34a127193ff43f979eb874be0827cc3ff8e6
AmolSB <amolborgaonkar333@gmail.com>
2018-07-25 21:34:15 +0530

    Complete 5th Module

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'event-thumbnail',
@@ -6,15 +6,28 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
   <div class="well hoverwell thumbnail">
       <h2>{{event.name}}</h2>
       <div>Data: {{event.data}}</div>
-      <div>Time: {{event.time}}</div>
+      <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event.price}}</div>
       <div>
         <span>Location: {{event.location.address}}</span>
         <span>&nbsp;</span>
         <span>{{event.location.city}}, {{event.location.country}}</span>
       </div>
+      <div>
+        Online URL: {{ event?.onlinUrl }}
+      </div>
     </div> 
-  `
+  `,
+  styles: [`
+    .thumbnail { min-height: 250px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,26 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+
+declare let toastr;
 
 @Component({
   selector: 'events-list',
   template: `<div>
   <h1>Upcoming Angular Events</h1>
   <hr />
-  <event-thumbnail [event] = "event1"></event-thumbnail>
+  <div class="row">
+    <div *ngFor="let event of events" class="col-md-5">
+      <event-thumbnail [event] = "event"></event-thumbnail>
+    </div>
+  </div>
   </div>
 `
 })
-export class EventsListComponent {
-  event1 = {
-    id: 1,
-    name: 'Angular Connect',
-    data: '9/29/2036',
-    time: '10:00 am',
-    price: 599.99,
-    imageUrl: '/assets/images/angular-connect-shield.png',
-    location: {
-      address: '1057 DT',
-      city: 'London',
-      country: 'England'
-    }
+export class EventsListComponent implements OnInit{
+  events: any[];
+
+  constructor(private eventService: EventService) {  }
+
+  ngOnInit() {
+    this.events = this.eventService.getEvents();
   }
+
 }
