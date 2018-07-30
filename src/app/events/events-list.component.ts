a3677a7819c5b8b3a4bd4b27df7858ed7c17109a
410082 <tarun.sekharamahanti@toyota.com>
2018-07-30 18:58:47 +0530

    Create model for event

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared/index';
 
 declare let toastr;
 
@@ -18,7 +19,7 @@ declare let toastr;
 `
 })
 export class EventsListComponent implements OnInit{
-  events: any;
+  events: IEvent[];
 
   constructor(private eventService: EventService, private route: ActivatedRoute) {  }
 
